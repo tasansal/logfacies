d05805c9ce944166f740ebcc441727cf684c5698
T. Altay Sansal <tasansal@outlook.com>
2018-08-12 21:03:16 -0500

    more edits

--- main.py
+++ main.py
@@ -21,13 +21,6 @@ from visualization import make_facies_log_plot
 
 close('all')
 
-FACIES_LABELS = {1:'SS',2: 'CSiS',3: 'FSiS',
-                 4:'SiSh', 5:'MS', 6:'WS',
-                 7:'D', 8:'PS', 9:'BS'}
-
-FACIES_COLORS = ['#F4D03F', '#F5B041','#DC7633','#6E2C00',
-    '#1B4F72','#2E86C1', '#AED6F1', '#A569BD', '#196F3D']
-
 set_option("display.max_rows", 10)
 options.mode.chained_assignment = None
 
