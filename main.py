1f9949cc032e97af525a927bc3ee723ed03cc8e0
T. Altay Sansal <tasansal@outlook.com>
2018-08-12 20:17:09 -0500

    Updated the viz, and Jupyter NB

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
-import numpy as np
+"""
+The nine discrete facies (classes of rocks) are:
+1 = Nonmarine sandstone
+2 = Nonmarine coarse siltstone
+3 = Nonmarine fine siltstone
+4 = Marine siltstone and shale
+5 = Mudstone (limestone)
+6 = Wackestone (limestone)
+7 = Dolomite
+8 = Packstone-grainstone (limestone)
+9 = Phylloid-algal bafflestone (limestone)
+"""
 
-print("Hello Jupyter!")
+from os.path import join
+
+from pandas import read_csv, options, set_option
+
+from matplotlib.pyplot import close
+
+from visualization import make_facies_log_plot
+
+close('all')
+
+FACIES_LABELS = {1:'SS',2: 'CSiS',3: 'FSiS',
+                 4:'SiSh', 5:'MS', 6:'WS',
+                 7:'D', 8:'PS', 9:'BS'}
+
+FACIES_COLORS = ['#F4D03F', '#F5B041','#DC7633','#6E2C00',
+    '#1B4F72','#2E86C1', '#AED6F1', '#A569BD', '#196F3D']
+
+set_option("display.max_rows", 10)
+options.mode.chained_assignment = None
+
+if __name__ == '__main__':
+    file_path = join('data_input', 'facies_vectors.csv')
+
+    well_data = read_csv(file_path)
+    well_data.set_index('Well Name', inplace=True)
+
+    well_names = well_data.index.unique()
+    num_wells = len(well_names)
+
+    print("There are 10 wells in *.csv file:")
+    [print("  - %s" % well_name) for well_name in well_names]
+
+    blind_well = well_data.loc['SHANKLE']
+    training_data = well_data.drop('SHANKLE')
+    training_wells = training_data.index.unique()
+
+    training_data.loc[:,'FaciesLabels'] = training_data.apply(lambda row: FACIES_LABELS[(row['Facies'])], axis=1)
+    training_data.describe()
+    # # Plot all wells
+    # for well in training_wells:
+    #     make_facies_log_plot(training_data.loc[well], FACIES_COLORS)
